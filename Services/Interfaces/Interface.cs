f2a21b876b8bff61fc5933e1a86d63079ef886a7
Z free <985463602@qq.com>
2024-04-08 11:18:59 +0800

    细化结构并新建了一些示例文件，把Mainwindows移动到Views目录下

---
+++ Services/Interfaces/Interface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoDemo.Services.Interfaces
+{
+    internal interface Interface
+    {
+    }
+}
---
+++ Services/Service.cs
@@ -0,0 +1,13 @@
+﻿using DemoDemo.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoDemo.Services
+{
+    internal class Service : Interface
+    {
+    }
+}
